29e2e2b7cfabce098ce625bae2c57f35d24130e5
Salvatore949 <salvo.mess49@gmail.com>
2021-11-09 09:26:40 +0100

    struttura

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+//Utilizzo il ciclo for per analizzare gli elementi presenti nell'array
+// Creo delle costanti
+// Utilizzo il query selector per collegare l'array con il div container
+//Attraverso innerHTML stampo in pagina le varie car
+//Utilizzo il filter per estrapolare e raggruppare dall'array gli elementi dello stesso colore diverso
+
 const icons = [
 	{
 		name: 'cat',
@@ -115,17 +121,34 @@ const icons = [
 
 console.log(icons)
 
-for ( i = 0; i < icons.lenght; i++ ){
-    let oggetto = icons[i];
-    Nome = oggetto.name;
-    prefisso = oggetto.prefix;
-    tipo = oggetto.type;
-    famiglia = oggetto.family;
-    colore = oggetto.color;
+for (let i = 0; i < icons.length; i++ ){
+    const oggetto = icons[i];
+    const nome = oggetto.name;
+    const prefisso = oggetto.prefix;
+    const tipo = oggetto.type;
+    const famiglia = oggetto.family;
+    const colore = oggetto.color;
 
-    document.querySelector('.container').innerHTML += `        
-    <div>
-        <i class = "${prefisso} ${tipo} ${famiglia} ${colore}"></i> alt="${Nome}"
-        <span>${Nome}</span>
+    document.querySelector('.container').innerHTML += `
+    <div class="card">
+       <div id="content">        
+            <i class = "${famiglia} ${prefisso}${nome} ${colore}" alt="${nome}"></i> 
+            <span>${nome}</span>
+       </div>
     </div>`  
 }
+
+const orange = icons.filter((oggetto) => {
+   return oggetto.color === "blue" ;      
+}
+);
+
+const green = icons.filter((oggetto) => {
+    return oggetto.color === "green" ;      
+ }
+ );
+
+ const blue = icons.filter((oggetto) => {
+    return oggetto.color === "blue" ;      
+ }
+ );
